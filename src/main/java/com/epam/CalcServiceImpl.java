5ecbdb7cf9720554fd29fdc5a5702ea18fc9b842
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 08:23:13 +0300

    01

---
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -0,0 +1,26 @@
+package com.epam;
+
+//QualityGateIssuesAndFailed test
+public class CalcServiceImpl implements CalcService {
+    @Override
+    public int add(int a, int b) {
+        var c = a + b;
+        return a + b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
